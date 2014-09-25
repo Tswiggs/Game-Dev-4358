0761a5535ae5ed2ded811a61a84ff3d7c3617cec
Swensen, Steven T <tyler.swensen@gmail.com>
2014-09-25 15:55:51 -0500

    Work in progress... creating camera script for pre launch activity

---
+++ MonsterMarbles/Assets/Scripts/LaunchCameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchCameraController : MonoBehaviour {
+
+	public float horizontalOffset = 1f;
+	public float verticalOffset = 1f;
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		//vector3 offset=(target.Position-horizontalOffset
+		transform.postion=target.position + 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
