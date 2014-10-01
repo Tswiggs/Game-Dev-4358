93fb1f7236592d8a36266f0b850a17e9b20373b4
John Montgomery <johnstephenmontgomery@gmail.com>
2014-09-30 23:27:56 -0500

    Added Main Menu scene
    - The Main Menu scene is dirt simple and uses two new assets, a Zugi_Roll_Title and a musical piece (CC-attribution license) called Open Those Bright Eyes
    - Updated Constants scripts, if you need to refer to a scene by name, you can use the constant!
    - Also added various fixes involving the presentaiton (sky bit counter, power increase on collisions)

---
+++ MonsterMarbles/Assets/Scripts/Camera Scripts/UncontrolledRotateCameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UncontrolledRotateCameraController : MonoBehaviour {
+
+	public GameObject target;
+
+	public float rotationSpeed = 75f;
+	public float rotateVerticalOffset = 0f;
+	public Vector3 rotationAxis = new Vector3 (0, 1, 0);
+	public int direction = 1;
+	
+	private float rotation;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotation= direction * Time.deltaTime * rotationSpeed;
+		//offset=Quaternion.Euler(0f, rotation, 0f)*offset;
+		//transform.position=player.transform.position + offset;
+		transform.RotateAround(target.transform.position, rotationAxis, rotation);
+		transform.LookAt(new Vector3(target.transform.position.x,target.transform.position.y+rotateVerticalOffset,target.transform.position.z));
+	}
+}
--- MonsterMarbles/Assets/Scripts/Constants.cs
+++ MonsterMarbles/Assets/Scripts/Constants.cs
@@ -6,4 +6,7 @@ public class Constants : MonoBehaviour {
 	public const string TAG_PLAYER = "Player";
 	public const string TAG_BUMPER = "Bumper";
 	public const string TAG_MARBLE = "Marble";
+
+	public const string SCENE_MAIN_MENU = "Main Menu";
+	public const string SCENE_PILA_PLAINS = "Pila_Plains_Prototype";
 }
