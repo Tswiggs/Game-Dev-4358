c5659f576b9d3300f8bd055f1d060869f54d4d90
Swensen, Steven T <tyler.swensen@gmail.com>
2014-10-02 17:21:19 -0500

    Continued developing the pre launch controls.

---
+++ MonsterMarbles/Assets/AimPlayerBall.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TouchScript.Gestures;
+using TouchScript.Gestures.Simple;
+using TouchScript.Utils;
+using UnityEngine;
+
+
+public class AimPlayerBall : MonoBehaviour {
+
+	public float aimSpeed=1;
+
+	private Quaternion lastLocalRotation;
+	private Quaternion localRotationToGo;
+
+	// Use this for initialization
+	void Awake () {
+		localRotationToGo = lastLocalRotation = transform.localRotation;
+	}
+
+	private void OnEnable()
+	{
+		if (GetComponent<SimplePanGesture>() != null) GetComponent<SimplePanGesture>().StateChanged += panStateChanged;
+	}
+	
+	private void OnDisable()
+	{
+		if (GetComponent<SimplePanGesture>() != null) GetComponent<SimplePanGesture>().StateChanged -= panStateChanged;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var fraction = aimSpeed*Time.deltaTime;
+		if (transform.localRotation != lastLocalRotation)
+		{
+			localRotationToGo = transform.localRotation;
+		}
+		transform.localRotation = lastLocalRotation = Quaternion.Lerp(transform.localRotation, localRotationToGo, fraction);
+	}
+
+	private void panStateChanged(object sender, GestureStateChangeEventArgs e)
+	{
+		switch (e.State)
+		{
+		case Gesture.GestureState.Began:
+		case Gesture.GestureState.Changed:
+			var gesture = (SimplePanGesture)sender;
+			
+			float deltaPos = gesture.LocalDeltaPosition.x;
+			if (deltaPos != 0) localRotationToGo.x += deltaPos;
+			break;
+		}
+	}
+}
