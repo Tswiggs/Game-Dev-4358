a73e24e68a392c57abadfd7bea1de5265f895525
unknown <tyler.swensen@gmail.com>
2014-11-08 10:47:12 -0600

    Working on the character root logic and the powers.

--- MonsterMarbles/Assets/Scripts/FollowPositionAndRotation.cs
+++ MonsterMarbles/Assets/Scripts/FollowPositionAndRotation.cs
@@ -17,9 +17,10 @@ public class FollowPositionAndRotation : MonoBehaviour {
 		{
 			transform.rotation=Quaternion.LookRotation(target.rigidbody.velocity);
 		}
-		else if(target.rotation.x<=90f && target.rotation.x >= 270)
+		else if(target.up.y>10f)
 		{
-			transform.rotation=target.transform.rotation;
+			Vector3 forward=new Vector3(target.forward.x, 0, target.forward.z);
+			transform.rotation=Quaternion.LookRotation(forward, Vector3.up);
 		}
 	}
 }
