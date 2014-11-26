c080d31d5db7dd7faebf42a2192468798f984c94
John Montgomery <mrmarth@gmail.com>
2014-11-25 23:07:39 -0600

    New set of assets, get them from dropbox. Updated Hotstreak model.

---
+++ MonsterMarbles/Assets/PullTestScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using TouchScript.Gestures;
+using System.Collections;
+using System;
+
+public class PullTestScript : MonoBehaviour {
+	
+	
+	private Vector2 startPosition;
+	
+	private GUIText debugText;
+	
+	// Use this for initialization
+	void Start () {
+		startPosition = new Vector2(0,0);
+		GetComponent<LongPressGesture>().LongPressed += pressHandler;
+		GetComponent<PanGesture>().PanStarted += panStartedHandler;
+		GetComponent<PanGesture>().Panned += pannedHandler;
+		GetComponent<PanGesture>().PanCompleted += panCompletedHandler;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	private void OnEnable(){
+		GetComponent<LongPressGesture>().LongPressed += pressHandler;
+		GetComponent<PanGesture>().PanStarted += panStartedHandler;
+		GetComponent<PanGesture>().Panned += pannedHandler;
+		GetComponent<PanGesture>().PanCompleted += panCompletedHandler;
+	}
+	
+	private void onDisable() {
+		
+	}
+	
+	private void pressHandler(object sender, EventArgs e)
+	{
+			this.transform.guiText.text = "Pressed Motherfucker";
+			Destroy(this);
+	}
+	
+	private void panStartedHandler(object sender, EventArgs e)
+	{
+		startPosition = GetComponent<PanGesture>().ScreenPosition;
+		Debug.Log(startPosition.y);
+	}
+	
+	private void pannedHandler(object sender, EventArgs e)
+	{
+		
+	}
+	
+	private void panCompletedHandler(object sender, EventArgs e)
+	{
+		
+	}
+}
