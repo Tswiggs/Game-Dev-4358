2da0bc94d419b48c42b777b09c3e17d0f4b458f0
John Montgomery <mrmarth@gmail.com>
2014-11-29 23:38:45 -0600

    Improved camera handling. Sky Bit trails. Try it!

--- MonsterMarbles/Assets/Scripts/RotateObject.cs
+++ MonsterMarbles/Assets/Scripts/RotateObject.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class RotateObject : MonoBehaviour {
 
 	public Vector3 axisSpeed;
+	
+	void Start(){
 
+	}
+	
 	void Update(){
 		transform.Rotate( axisSpeed * Time.deltaTime);
 	}
--- MonsterMarbles/Assets/SkyBitCollisionHandler.cs
+++ MonsterMarbles/Assets/SkyBitCollisionHandler.cs
@@ -1,24 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MarbleCollisionHandler : MonoBehaviour {
-
-
+public class SkyBitCollisionHandler : MonoBehaviour {
+	
+	
 	public AudioSource audioSource;
-
+	
 	public float onCollisionPowerIncrease;
 	public float onCollisionPowerMultiplier;
 	
 	public bool useDefaults = true;
 	
-	public static float DEFAULT_COLLISION_POWER_INCREASE = 10;
-	public static float DEFAULT_COLLISION_POWER_MULTIPLIER = 10;
-
+	public Color currentColor;
+	private int collisionCount = 0;
+	private float collisionTimer = 0f;
+	
+	public static float DEFAULT_COLLISION_POWER_INCREASE = 10f;
+	public static float DEFAULT_COLLISION_POWER_MULTIPLIER = 10f;
+	
+	public static float COLLISION_DECAY_TIME = 3f;
+	
 	// Use this for initialization
 	void Start () {
-	
+		updateCollisionColor();
 	}
-
+	
+	void updateCollisionColor(){
+		if(collisionCount == 0){
+			currentColor = Color.white;
+		}
+		else if(collisionCount == 1){
+			currentColor = Color.cyan;
+		}
+		else if(collisionCount == 2){
+			currentColor = Color.blue;
+		}
+		else if(collisionCount >= 3){
+			currentColor = Color.magenta;
+		}
+		
+		GetComponent<ParticleSystem>().startColor = currentColor;
+		this.transform.FindChild("Particle Streak").GetComponent<ParticleSystem>().startColor = currentColor;
+	}
+	
 	void OnCollisionEnter(Collision collision) {
 		if (collision.collider.CompareTag(Constants.TAG_PLAYER) || collision.collider.CompareTag (Constants.TAG_MARBLE)) {
 			if(audioSource != null){
@@ -28,7 +52,9 @@ public class MarbleCollisionHandler : MonoBehaviour {
 			Vector3 forceVector = collision.relativeVelocity *rigidbody.mass;
 			forceVector.Scale(new Vector3(getPowerMultiplier(),getPowerMultiplier(),getPowerMultiplier()));
 			rigidbody.AddForce(forceVector);
-
+			
+			incrementCollisionCount();
+			
 		}
 		if (collision.collider.CompareTag(Constants.TAG_BUMPER)) {
 			Vector3 forceVector = collision.contacts[0].normal;
@@ -37,8 +63,9 @@ public class MarbleCollisionHandler : MonoBehaviour {
 			//forceDirection.Scale(new Vector3(-1,0,-1));
 			rigidbody.velocity = forceVector * (rigidbody.velocity.magnitude+getPowerIncrease());
 			//rigidbody.AddForce(forceVector);
+			incrementCollisionCount();
 		}
-
+		
 		
 	}
 	
@@ -60,8 +87,21 @@ public class MarbleCollisionHandler : MonoBehaviour {
 		}
 	}
 	
+	void incrementCollisionCount(){
+		collisionCount += 1;
+		collisionTimer = 0f;
+		updateCollisionColor();
+	}
+	
 	// Update is called once per frame
 	void Update () {
-	
+		if(collisionCount > 0){
+			collisionTimer += Time.deltaTime;
+			if(collisionTimer >= COLLISION_DECAY_TIME){
+				collisionCount = 0;
+				updateCollisionColor();
+				collisionTimer = 0f;
+			}
+		}
 	}
 }
