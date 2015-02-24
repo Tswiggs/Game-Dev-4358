a74ac8f0c71bfc0ae8e6a83845c61252f62c5960
unknown <tyler.swensen@gmail.com>
2015-02-23 21:17:46 -0600

    +Rigged up FrostwindMt so that it is now fully functional! +Tweaked aim responsiveness

---
+++ MonsterMarbles/Assets/Scripts/whirlwind_effect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whirlwind_effect : MonoBehaviour {
+	public float upStrength=10f;
+	public float forwardStrength=10f;
+	void OnTriggerStay(Collider other) {
+		Vector3 direction = other.rigidbody.velocity;
+		direction.Set(direction.x, 0, direction.z);
+		direction=direction*forwardStrength;
+		direction+=Vector3.up*upStrength;
+		other.gameObject.rigidbody.AddForce(direction);
+	}
+}
